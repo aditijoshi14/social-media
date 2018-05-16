f62fd7c09c2b38dee26b5e2edfe34a85d079a692
joshiad <Aditi@Aditis-MBP.fios-router.home>
2018-05-16 18:25:20 -0400

    Added global colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignInComponent
+    SignInComponent,
+    NavBarComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
